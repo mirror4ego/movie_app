705c683bc9254ff043adfb0b2035420183cb4e54
mirror4ego <mirror4ego@naver.com>
2019-07-03 07:17:02 +0900

    최종 완성

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/51vpnbwFHrL._SY445_.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "OldBoy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    this._getMovies();
   }
-]
 
-const App = () => {
-  return (
-    <div className="App">
-      {movies.map((movie, index) =>
-        <Movie title={movie.title} poster={movie.poster} key={index}/>
-      )}
-    </div>
-  );
-};
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) =>
+      <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres} 
+      synopsis={movie.synopsis}
+      />
+    )
+    return movies;
+  }
 
-      // 반복문 : 리턴을 넣는 방식
-      // {movies.map(movie => {
-      //    return <Movie title={movie.title} poster={movie.poster} />
-      // })}
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json')
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const { movies } =this.state;  
+    return (
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+
+
+// 반복문 : 리턴을 넣는 방식
+// {movies.map(movie => {
+//    return <Movie title={movie.title} poster={movie.poster} />
+// })}
 
 export default App;
